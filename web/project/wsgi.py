baad5b02d1219364fd3928ff48bd6697f5ea2349
Compeito <com0806peito@icloud.com>
2019-07-17 11:57:57 +0900

    fix: settingsファイルの環境変数修正

--- web/project/wsgi.py
+++ web/project/wsgi.py
@@ -1,5 +1,7 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
+settings = 'project.settings.' + ('dev' if os.environ['DEBUG'] == 'true' else 'prod')
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 application = get_wsgi_application()
