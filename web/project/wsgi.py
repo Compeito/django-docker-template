878f508a323b56eb50f1470806265599d73be127
Compeito <com0806peito@icloud.com>
2019-09-15 23:05:55 +0900

    feat: ipからsockへの移行と設定の調整

--- web/project/wsgi.py
+++ web/project/wsgi.py
@@ -3,5 +3,5 @@ from django.core.wsgi import get_wsgi_application
 
 settings = 'project.settings.' + ('dev' if os.environ['DEBUG'] == 'true' else 'prod')
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings)
 application = get_wsgi_application()
